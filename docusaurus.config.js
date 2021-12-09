72841fe025d86de2fe8fbf08c93e748c78e5b6cd
Herman Streltsov <mail@streltsov.dev>
2021-12-09 17:41:53 +0300

    Set trailingSlash to true

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,6 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 module.exports = {
   title: 'Herman Streltsov',
+  trailingSlash: true,
   url: 'https://blog.streltsov.dev',
   baseUrl: '/blog/',
   onBrokenLinks: 'throw',
